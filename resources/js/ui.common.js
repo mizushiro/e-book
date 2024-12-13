e22358726a63cf14a89f18acef0b53dac4c91479
mizushiro <39608160+mizushiro@users.noreply.github.com>
2024-12-13 11:08:28 +0900

    로그인 회원가입

---
+++ resources/js/ui.common.js
@@ -0,0 +1,12 @@
+ document.addEventListener('DOMContentLoaded', () => {
+    UI.callback.nav = (v) => {
+        console.log(v.state);
+        const wrap = document.querySelector('html');
+
+        if (v.state === 'true') {
+            wrap.dataset.nav = 'open';
+        } else {
+            wrap.dataset.nav = 'close';
+        }
+    }
+});
